e8afbb96c816ab435c690a6834547dab5cb3ddca
kyle-skysmith <95176233+kyle-skysmith@users.noreply.github.com>
2023-08-05 11:07:47 -0400

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,14 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's Play :]")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+     print('Correct!')
+else:
+    print("Incorrect!")
